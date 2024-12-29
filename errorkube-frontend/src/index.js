cb4bcc9b02ecd488449a70c58501045c7dec40e9
ksanthosh20ka1a0554 <ksanthosh20ka1a0554@gmail.com>
2024-12-29 15:05:23 +0530

    added manifests folder and files

--- errorkube-frontend/src/index.js
+++ errorkube-frontend/src/index.js
@@ -8,13 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div style={{ display : "flex", backgroundColor :"#d4eaf7"}}>
-              <img src="/images/Error_Kube-logo.png" style={{ width:"100px", padding: "5px"}} alt="BigCo Inc. logo"/>
+              <img src="/images/Error_Kube-logo.png" style={{ width:"100px", padding: "5px"}} alt="ErrorKube logo"/>
               </div>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
